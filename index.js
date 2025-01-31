f4dbef8d8e46fa3cf8197d5f060c1d0b9a40bcde
Ville Ylikangas <villeylik@gmail.com>
2025-01-31 08:56:57 +0200

    Add functionality for number and operator buttons

--- index.js
+++ index.js
@@ -7,8 +7,32 @@ const operations = {
 
 let num1;
 let num2;
-let operator;
+const numpad = document.getElementById('numpad');
+const operators = document.getElementById('operators');
+const operator = document.getElementById('operator');
+const operand1 = document.getElementById('firstNum');
+const operand2 = document.getElementById('secondNum');
+let isSecondOperator = false;
 
 const operate = (operator, x, y) => {
   return operations[operator] ? operations[operator](x, y) : 'Invalid operator';
 };
+
+numpad.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const num = event.target.dataset.value;
+    if (!isSecondOperator) {
+      operand1.textContent += num;
+      num1 = Number(operand1.textContent);
+    } else {
+      operand2.textContent += num;
+      num2 = Number(operand2.textContent);
+    }
+  }
+});
+operators.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    operator.textContent = event.target.dataset.value;
+    isSecondOperator = true;
+  }
+});
